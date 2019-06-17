526af3dae7d9a260b2b66086ba2efc71fbf49884
glennwester <glenn.wester@gmail.com>
2019-06-17 09:30:33 -0700

    Init
    Forecasting

---
+++ NOAAWeather/NOAAWeather/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NOAAWeather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
